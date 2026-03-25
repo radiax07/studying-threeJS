8cdae88c262b4ee0b9676be7237f507cb4846031
Rishi <dasranbirkumar173@gmail.com>
2026-03-25 08:50:34 +0530

    camera initialized for cube

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 import * as THREE from 'three'
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene() // it is a class so we have to use new
 
-const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
+const cubeGeometry = new THREE.BoxGeometry(1,1,1) // This is the geometry
+const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"}) // This is basic mesh, does not interact with light
 
 const cubeMesh = new THREE.Mesh(
     cubeGeometry,
     cubeMaterial
 )
+
+scene.add(cubeMesh) // we have to explicitly say that this is the child of the scene
+
+// initialize camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 30) // (FOV, Aspect Ratio, camera near, camera far)
+
+// camera near -> anything near than this it won't be visible
+// camera far -> anything further than this it won't be visible
+// console.log(window.innerWidth / window.innerHeight)
+
+// position the camera
+
+camera.position.z = 5
+
+scene.add(camera)
